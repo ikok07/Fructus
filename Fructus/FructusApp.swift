96914c5eb6abd72bdd5f53052f7bb9c93cb8d63c
Kaloyan Stefanov <kokmarok@gmail.com>
2023-07-26 18:22:27 +0300

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Kaloyan Petkov on 26.07.23.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
